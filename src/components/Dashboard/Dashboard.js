0cb02f04cdeb495d7ecabd61a861d99ef6f3aee4
Steve Hunley <shunley2000@yahoo.com>
2021-01-26 14:07:09 -0600

    Initial layout

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Menu from 'components/Menu'
+
+const Dashboard = () => {
+  return (
+    <main role='main'>
+      <Menu />
+      <div>Content</div>
+      <footer>Footer</footer>
+    </main>
+  )
+}
+
+export default Dashboard
