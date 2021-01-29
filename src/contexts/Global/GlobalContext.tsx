a934af3ebf37e7133e30c2c16349d783bb6561c8
Steve Hunley <shunley2000@yahoo.com>
2021-01-29 16:21:55 -0600

    Added more TypeScript

--- src/contexts/Global/GlobalContext.tsx
+++ src/contexts/Global/GlobalContext.tsx
@@ -1,13 +1,12 @@
 import React, { createContext } from 'react'
 import useDashboardReducer from 'reducers/useDashboardReducer'
 import PropTypes from 'prop-types'
-import { DashboardState, DashboardAction, ContextProps } from 'typings/dashboard';
+import { ContextProps } from 'typings/dashboard'
+import { ChildrenProps } from 'typings/global.d'
 
+export const Context = createContext<Partial<ContextProps>>({})
 
-
-export const Context = createContext({} as ContextProps)
-
-export const Provider = ({ children }) => {
+export const Provider = ({ children }: ChildrenProps) => {
   const [dashboardState, dashboardDispatch] = useDashboardReducer()
 
   return (
--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -1,3 +1,8 @@
+import { ReactNode } from 'react'
+
 export type NullableString = string | null | undefined
 export type NullableBoolean = boolean | null | undefined
 export type NullableNumber = number | null | undefined
+export interface ChildrenProps {
+  children: ReactNode
+}
