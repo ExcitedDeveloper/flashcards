69738f3233bf378c2deaf5ac50c2151e8cc87989
Steve Hunley <shunley2000@yahoo.com>
2021-02-03 20:24:25 -0600

    Navbar changes

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,57 +1,55 @@
-import React, { useState, useContext } from 'react'
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
-} from 'reactstrap'
-import { GlobalContext } from 'contexts/Global'
+/* eslint-disable jsx-a11y/anchor-is-valid */
 
 const Menu = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const { dashboardState } = useContext(GlobalContext)
-
-  const toggle = () => setIsOpen(!isOpen)
+  const toggleMenu = () => {
+    const navToggle = document.getElementsByClassName('toggle')
+    for (let i = 0; i < navToggle.length; i += 1) {
+      navToggle?.item(i)?.classList.toggle('hidden')
+    }
+  }
 
   return (
-    <>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>{dashboardState?.mode}</NavbarText>
-        </Collapse>
-      </Navbar>
-    </>
+    <nav className="flex flex-wrap items-center justify-between bg-blue-200">
+      <div className="flex md:hidden">
+        <button id="hamburger" type="button" onClick={toggleMenu}>
+          <img
+            className="toggle block"
+            src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png"
+            width="40"
+            height="40"
+            alt="toggle show"
+          />
+          <img
+            className="toggle hidden"
+            src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png"
+            width="40"
+            height="40"
+            alt="toggle hide"
+          />
+        </button>
+      </div>
+      <div className="toggle hidden md:flex w-full md:w-auto text-right text-bold md:mt-0 border-t-2 border-blue-900 md:border-none">
+        <a
+          href="#"
+          className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none"
+        >
+          File
+        </a>
+        <a
+          href="#"
+          className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none"
+        >
+          Options
+        </a>
+        <a
+          href="#"
+          className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none"
+        >
+          Help
+        </a>
+      </div>
+      <p className="text-blue-900 pr-3">Card List</p>
+    </nav>
   )
 }
 
